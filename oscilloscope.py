fd0f2dee8493fc8f5033f6bca2d4c9cd2d696e18
ngpaladi <ngpaladi@users.noreply.github.com>
2022-06-24 17:34:56 -0400

    More Auxiliary Classes

--- oscilloscope.py
+++ oscilloscope.py
@@ -1,4 +1,5 @@
 import pyvisa as visa
+from enum import Enum
 
 class TriggerStatus:
     def __init__(self, internal_state_register:int) -> None:
@@ -6,13 +7,36 @@ class TriggerStatus:
         self.triggered = bool((internal_state_register & 1) !=0 )
         return
 
+class ChannelType(Enum):
+    AC = auto()
+    DC = auto()
+
+class Channel:
+    def __init__(self, id:int, name:str, units:str, division:float, offset:float, ) -> None:
+        self.name = name
+        self.id = id
+        self.units = units
+        self.division = division
+        return
+
+class Waveform:
+    def __init__(self, data:str, channel:Channel) -> None:
+        self.values = []
+        self.units = channel.units
+        self.interval = 0.
+        self.interval_units = 0.
+        return
+
 class Oscilloscope:
     def __init__(self, connection_string: str) -> None:
         self.scpi = self.resource_manager.open_resource(connection_string)
         self.scpi.read_termination = '\n'
         self.scpi.write_termination = '\n'
-        self.setup()
-        
+        self.channels = {}
+
+    def add_channel(self, channel:Channel) -> Channel:
+
+
     def write(self, string: str) -> str:
         return self.scpi.write(string)
 
@@ -22,9 +46,13 @@ class Oscilloscope:
     def read(self, string: str) -> str:
         return self.scpi.read(string)
     
-    def trigger_status(self) -> dict:
+    def trigger_status(self) -> TriggerStatus:
         return TriggerStatus(int(self.scpi.query("INR?")))
     
-    def setup(self) -> None:
-        self.write("CHDR OFF")
+    def waveform(self, channel_id:int) -> Waveform:
+        return Waveform()
+
+    
+    def initialize(self) -> None:
+        self.scpi.write("CHDR OFF")
         return
